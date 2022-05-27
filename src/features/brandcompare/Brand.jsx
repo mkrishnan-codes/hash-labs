dd0a5cb3c173cdbe65f63fd767a96fa0ac60a4eb
Manu Krishnan <manu.krishnan@falconx.io>
2022-05-27 22:38:25 +0530

    t1

--- src/features/brandcompare/Brand.jsx
+++ src/features/brandcompare/Brand.jsx
@@ -1,4 +1,4 @@
-import { Box, Paper, TextField } from '@mui/material'
+import {  Grid, Paper, TextField } from '@mui/material'
 import React, { useMemo } from 'react'
 const calc = (w, p) => p !== 0 ? (w / p).toFixed(2) : "";
 export default function Brand({ id, brand,
@@ -12,19 +12,52 @@ export default function Brand({ id, brand,
 
     return (
 
-        <Box
-            component="form"
-            noValidate
-            autoComplete="off"
-        >
-            <Paper elevation={2} sx={{ p: 4, m: 1 }}>
-                    <TextField id="inpBrand" label="Brand" variant="standard" value={brand} onChange={(evt) => onUpdate('brand', id, evt.currentTarget.value)} />
-                <div>
-                    <TextField type="number" id="inpWeightUnits" label="Weight Units" variant="standard" value={weightUnits} onChange={(evt) => onUpdate('weightUnits', id, evt.currentTarget.value)} />
-                    <TextField type="number" id="inpPrice" label="Price" variant="standard" value={price} onChange={(evt) => onUpdate('price', id, evt.currentTarget.value)} />
-                </div>
-                <TextField type="number" id="inpPricePerUnit" label="Price per unit" variant="standard" value={calculation} />
-            </Paper >
-        </Box>
+        //     <Paper elevation={2} sx={{ p: 4, m: 1 }}>
+        //    <Stack
+        //   component="form"
+        //   spacing={2}
+        //   noValidate
+        //   autoComplete="off"
+        // >
+
+        //                 <TextField id="inpBrand" label="Brand" variant="standard" value={brand} onChange={(evt) => onUpdate('brand', id, evt.currentTarget.value)} />
+        //             <Box component="span">
+        //                 <TextField type="number" id="inpWeightUnits" label="Weight Units" variant="standard" value={weightUnits} onChange={(evt) => onUpdate('weightUnits', id, evt.currentTarget.value)} />
+        //                 <TextField type="number" id="inpPrice" label="Price" variant="standard" value={price} onChange={(evt) => onUpdate('price', id, evt.currentTarget.value)} />
+        //             </Box>
+        //             <TextField type="number" id="inpPricePerUnit" label="Price per unit" variant="standard" value={calculation} />
+        //     </Stack>
+        //         </Paper >
+        <Paper elevation={2} sx={{ p: 4, m: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField fullWidth id="inpBrand" label="Brand" placeholder="Eg: Unilever" variant="standard" value={brand} onChange={(evt) => onUpdate('brand', id, evt.currentTarget.value)} />
+
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth type="number" id="inpWeightUnits" label="Weight Units" variant="standard" value={weightUnits} onChange={(evt) => onUpdate('weightUnits', id, evt.currentTarget.value)} />
+
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField fullWidth type="number" id="inpPrice" label="Price" variant="standard" value={price} onChange={(evt) => onUpdate('price', id, evt.currentTarget.value)} />
+
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth type="number" id="inpPricePerUnit" label="Price per unit" variant="filled" contentEditable={false} value={calculation} />
+
+                </Grid>
+            </Grid>
+        </Paper >
+
+
     )
 }
+//  <Stack
+//       component="form"
+//       sx={{
+//         width: '25ch',
+//       }}
+//       spacing={2}
+//       noValidate
+//       autoComplete="off"
+//     >
--- src/features/brandcompare/BrandCompare.jsx
+++ src/features/brandcompare/BrandCompare.jsx
@@ -2,6 +2,8 @@ import { Box, Fab } from '@mui/material'
 import React, { useCallback, useState } from 'react'
 import Brand from './Brand';
 import AddIcon from '@mui/icons-material/Add';
+import HelpModal from './HelpModal';
+
 const getBrand = (id) => ({
     id,
     brand: String.fromCharCode(97 + id),
@@ -10,8 +12,22 @@ const getBrand = (id) => ({
     pricePerUnit: 0
 }
 )
+
+const fabStyle = {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+  };
 function BrandCompare() {
     const [brands, setbrands] = useState([getBrand(0)]);
+    const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = useCallback(() => {
+    setOpen(true);
+  },[open]);
+  const handleClose =  useCallback(() => {
+    setOpen(false);
+  },[open]);
     const onUpdate = useCallback((attr, id, value) => {
         let newArr = [...brands];
         newArr[id] = { ...newArr[id], [attr]: value }
@@ -30,9 +46,9 @@ function BrandCompare() {
         <Box sx={{ flexGrow: 1, p: 4 }}>
         
                 {brands.map((brnd) => <Brand key={brnd.id} {...brnd} onUpdate={onUpdate} />)}
-     
+        <HelpModal/>
             <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Fab color="primary" aria-label="add" onClick={add}>
+                <Fab color="primary" sx={fabStyle} aria-label="add" onClick={add}>
                     <AddIcon />
                 </Fab>
 
