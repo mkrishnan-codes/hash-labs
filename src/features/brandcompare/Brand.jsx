31a389477a066a1f3c0db080f10538139291a2dc
Manu Krishnan <manu.krishnan@falconx.io>
2022-05-28 19:04:14 +0530

    fix equation

--- src/features/brandcompare/Brand.jsx
+++ src/features/brandcompare/Brand.jsx
@@ -1,6 +1,6 @@
-import {  Grid, Paper, TextField } from '@mui/material'
+import { Grid, Paper, TextField } from '@mui/material'
 import React, { useMemo } from 'react'
-const calc = (w, p) => p !== 0 ? (w / p).toFixed(2) : "";
+const calc = (w, p) => w !== 0 ? (p / w).toFixed(2) : "";
 export default function Brand({ id, brand,
     weightUnits,
     price,
@@ -12,22 +12,6 @@ export default function Brand({ id, brand,
 
     return (
 
-        //     <Paper elevation={2} sx={{ p: 4, m: 1 }}>
-        //    <Stack
-        //   component="form"
-        //   spacing={2}
-        //   noValidate
-        //   autoComplete="off"
-        // >
-
-        //                 <TextField id="inpBrand" label="Brand" variant="standard" value={brand} onChange={(evt) => onUpdate('brand', id, evt.currentTarget.value)} />
-        //             <Box component="span">
-        //                 <TextField type="number" id="inpWeightUnits" label="Weight Units" variant="standard" value={weightUnits} onChange={(evt) => onUpdate('weightUnits', id, evt.currentTarget.value)} />
-        //                 <TextField type="number" id="inpPrice" label="Price" variant="standard" value={price} onChange={(evt) => onUpdate('price', id, evt.currentTarget.value)} />
-        //             </Box>
-        //             <TextField type="number" id="inpPricePerUnit" label="Price per unit" variant="standard" value={calculation} />
-        //     </Stack>
-        //         </Paper >
         <Paper elevation={2} sx={{ p: 4, m: 1 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
