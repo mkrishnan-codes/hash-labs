1d9bb0e2aaa36acf72ee683aba289abfc21493ef
Manu Krishnan <manu.krishnan@falconx.io>
2022-10-26 13:05:54 +0530

    notes and theme updates

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { HashRouter, Route, Switch } from "react-router-dom";
 import { routes } from './router-config';
 import AppLayout from '../layout/AppLayout';
+import { Box, LinearProgress } from '@mui/material';
+
+const Loader = ()=>{
+	return <Box sx={{ width: '100%' }}>
+	<LinearProgress />
+  </Box>
+}
+
 const RouteWithSubRoutes = (route) => {
 	return (
 		<AppLayout inner={route.inner}>
+			<Suspense fallback={<Loader/>}>
 			<Route
 				path={route.path}
 				render={props => (
@@ -12,6 +21,7 @@ const RouteWithSubRoutes = (route) => {
 					<route.component {...props} routes={route.routes} />
 				)}
 			/>
+			</Suspense>
 		</AppLayout>
 	);
 }
